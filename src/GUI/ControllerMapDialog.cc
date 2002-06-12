b0e6095edc17b21d7b751c54d7dea3cd3a640b9a
nixx2097 <>
2002-06-12 16:22:24 +0000

    added required "null" entry for controller config.

--- src/GUI/ControllerMapDialog.cc
+++ src/GUI/ControllerMapDialog.cc
@@ -22,6 +22,7 @@ ControllerMapDialog::ControllerMapDialog( int pipe_d, MidiController & mc,
 	add( table );
 	
 	list<string> gl;
+	gl.push_back( "null" );
 	for (gint p=0; p<128; p++){
 		string p_name = preset_controller->getCurrentPreset().getParameter(p).getName();
 		if ( p_name != "unused" ) gl.push_back( p_name );
