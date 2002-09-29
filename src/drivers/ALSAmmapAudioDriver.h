8c7896bd11f0bd49825f479a501cc94029e02e68
nixx2097 <>
2002-09-29 13:23:02 +0000

    fixed compilation error on non-ALSA sytstems

--- src/drivers/ALSAmmapAudioDriver.h
+++ src/drivers/ALSAmmapAudioDriver.h
@@ -14,32 +14,31 @@
 #define BUFSIZE 64
 
 class ALSAmmapAudioDriver : public AudioDriver {
+public:
+		ALSAmmapAudioDriver();
+	virtual	~ALSAmmapAudioDriver();
+	int 	open(){ return -1; };
+	int	open( Config & config );
+	void	close();
+	int	write(float *buffer, int frames);
+	int	setChannels(int channels);
+	int	setRate(int rate);
+	int	setRealtime();
 
-  public:
-    ALSAmmapAudioDriver();
-    virtual ~ ALSAmmapAudioDriver();
-    int open(){ return -1; };
-	int open( Config & config );
-    void close();
-    int write(float *buffer, int frames);
-    int setChannels(int channels);
-    int setRate(int rate);
-    int setRealtime();
-
-  private:
-    int _dsp_handle;
-    int _rate;
-    int _channels;
-    int _format;
-	unsigned char *audiobuf;
+private:
+	int 	xrun_recovery();
+	
+	int		_dsp_handle;
+	int		_rate;
+	int		_channels;
+	int		_format;
+	unsigned char	*audiobuf;
 #ifdef _ALSA
-	snd_pcm_t *playback_handle;
-    snd_pcm_hw_params_t *hw_params;
-    snd_pcm_sw_params_t *sw_params;
-	int xrun_recovery();
-	int	err;
-	unsigned periods;
-
+	snd_pcm_t		*playback_handle;
+	snd_pcm_hw_params_t	*hw_params;
+	snd_pcm_sw_params_t	*sw_params;
+	int			err;
+	unsigned		periods;
 #endif
 };
 
