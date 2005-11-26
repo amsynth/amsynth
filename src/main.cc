ad8dc87e97325878395db17a34effdb7560fedfe
Nick Dowell <nickdowell@gmail.com>
2005-11-26 11:45:31 +0000

    removed main.h

--- src/main.cc
+++ src/main.cc
@@ -2,13 +2,49 @@
  * (c) 2001-2005 Nick Dowell
  */
 
-#include "main.h"
+#include "MidiController.h"
+#include "VoiceAllocationUnit.h"
+#include "AudioOutput.h"
+#include "JackOutput.h"
+#include "GUI/GUI.h"
+#include "GUI/ParameterView.h"
+#include "Config.h"
+#include "base.h"
 #include "../config.h"
 
 #include <gtkmm/main.h>
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
+#include <string>
+
+
+PresetController *presetController;
+MidiController *midi_controller;
+VoiceAllocationUnit *vau;
+GenericOutput *out;
+Config config;
+GUI *gui;
+
+
+string help_text =
+"usage: amSynth [options]\n\
+\n\
+any options given here override those in the config file ($HOME/.amSynthrc)\n\
+\n\
+options:\n\
+-b <filename>	use <filename> as the bank to store presets,\n\
+		default = ~/.amSynth.presets\n\
+-m device	set the midi driver to use [alsa/oss/auto(default)]\n\
+-c channel	set the midi channel to respond to (default=all)\n\
+-a device	set the sound output driver to use [alsa/oss/auto(default)]\n\
+-r rate		set the sampling rate to use\n\
+-p voices	set the polyphony (maximum active voices)\n\
+-v		show version.\n\
+-d		show some debugging output\n\
+-h		show this usage message\n";
+
+
 
 int the_pipe[2];
 
