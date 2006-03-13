458f7414e3f97fe76c5800701bc530cb639f6d73
Nick Dowell <nickdowell@gmail.com>
2006-03-13 23:14:36 +0000

    folded ptest back into main executable

--- src/main.cc
+++ src/main.cc
@@ -18,6 +18,10 @@
 #include <fstream>
 #include <unistd.h>
 #include <string>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
 
 using namespace std;
 
@@ -44,6 +48,7 @@ options:\n\
 -p voices	set the polyphony (maximum active voices)\n\
 -v		show version.\n\
 -d		show some debugging output\n\
+-z		run a performance benchmark\n\
 -h		show this usage message\n";
 
 
@@ -108,6 +113,8 @@ int fcopy (const char * dest, const char *source)
 	return 0;
 }
 
+void ptest ();
+
 int main( int argc, char *argv[] )
 {
 	BrInitError br_err;
@@ -127,7 +134,7 @@ under certain conditions; see the file COPYING for details\n";
 	bool jack = false;
 	
 	int opt;
-	while( (opt=getopt(argc, argv, "vhstdm:c:a:r:p:b:"))!= -1 ) {
+	while( (opt=getopt(argc, argv, "vhstdzm:c:a:r:p:b:"))!= -1 ) {
 		switch(opt) {
 			case 'v':
 				cout << "amSynth " << VERSION << " -- compiled "
@@ -135,6 +142,9 @@ under certain conditions; see the file COPYING for details\n";
 				return 0;
 			case 'h':
 				cout << help_text; 
+				return 0;
+			case 'z':
+				ptest();
 				return 0;
 			default:
 				break;
@@ -322,3 +332,52 @@ under certain conditions; see the file COPYING for details\n";
 	delete out;
 	return 0;
 }
+
+///////////////////////////////////////////////////////////////////////////////
+
+void ptest ()
+{
+	//
+	// test parameters
+	// 
+	const int kTestBufSize = 256;
+	const int kTestSampleRate = 44100;
+	const int kTimeSeconds = 60;
+	const int kNumVoices = 10;
+
+	float *buffer = new float [kTestBufSize];
+
+	VoiceAllocationUnit *vau = new VoiceAllocationUnit;
+	vau->SetSampleRate (kTestSampleRate);
+	
+	// trigger off some notes for amSynth to render.
+	for (int v=0; v<kNumVoices; v++) vau->noteOn (60+v, 127);
+	
+	struct rusage usage_before; 
+	getrusage (RUSAGE_SELF, &usage_before);
+	
+	long total_samples = kTestSampleRate * kTimeSeconds;
+	long total_calls = total_samples / kTestBufSize;
+	long remain_samples = total_samples % kTestBufSize;
+	for (int i=0; i<total_calls; i++) vau->Process (buffer, buffer, kTestBufSize);
+	vau->Process (buffer, buffer, remain_samples);
+
+	struct rusage usage_after; 
+	getrusage (RUSAGE_SELF, &usage_after);
+	
+	unsigned long user_usec = (usage_after.ru_utime.tv_sec*1000000 + usage_after.ru_utime.tv_usec)
+							- (usage_before.ru_utime.tv_sec*1000000 + usage_before.ru_utime.tv_usec);
+	
+	unsigned long syst_usec = (usage_after.ru_stime.tv_sec*1000000 + usage_after.ru_stime.tv_usec)
+							- (usage_before.ru_stime.tv_sec*1000000 + usage_before.ru_stime.tv_usec);
+
+	unsigned long usec_audio = kTimeSeconds * kNumVoices * 1000000;
+	unsigned long usec_cpu = user_usec + syst_usec;
+	
+	fprintf (stderr, "user time: %f		system time: %f\n", user_usec/1000000.f, syst_usec/1000000.f);
+	fprintf (stderr, "performance index: %f\n", (float) usec_audio / (float) usec_cpu);
+	
+	delete [] buffer;
+	delete vau;
+}
+
