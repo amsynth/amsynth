62da2795b85fade80b1252de6b4094c004e672a5
Nick Dowell <nickdowell@gmail.com>
2006-05-12 11:38:52 +0000

    CoreAudio support

--- src/drivers/CoreMidiInterface.cc
+++ src/drivers/CoreMidiInterface.cc
@@ -7,6 +7,115 @@
 
 #if (__APPLE__)
 
+#pragma mark - CoreAudio
+
+#include <vector>
+#include <CoreAudio/CoreAudio.h>
+
+#include "../VoiceAllocationUnit.h"
+
+OSStatus audioDeviceIOProc (   AudioDeviceID           inDevice,
+							   const AudioTimeStamp*   inNow,
+							   const AudioBufferList*  inInputData,
+							   const AudioTimeStamp*   inInputTime,
+							   AudioBufferList*        outOutputData,
+							   const AudioTimeStamp*   inOutputTime,
+							   void*                   inClientData)
+{
+	float* outL;
+	float* outR;
+	unsigned numSampleFrames = 0;
+	unsigned stride = 1;
+	
+	if (1 < outOutputData->mBuffers[0].mNumberChannels)
+	{
+		numSampleFrames = outOutputData->mBuffers[0].mDataByteSize / (outOutputData->mBuffers[0].mNumberChannels * sizeof(float));
+		stride = outOutputData->mBuffers[0].mNumberChannels;
+		outL = (float*)outOutputData->mBuffers[0].mData;
+		outR = outL + 1;
+	}
+	else if (1 < outOutputData->mNumberBuffers)
+	{
+		numSampleFrames = outOutputData->mBuffers[0].mDataByteSize / (outOutputData->mBuffers[0].mNumberChannels * sizeof(float));
+		outL = (float*)outOutputData->mBuffers[0].mData;
+		outR = (float*)outOutputData->mBuffers[1].mData;
+	}
+	else
+	{
+		return kAudioDeviceUnsupportedFormatError;
+	}
+	
+	VoiceAllocationUnit** vau = (VoiceAllocationUnit **)inClientData;
+	if (*vau) (*vau)->Process(outL, outR, numSampleFrames, stride);
+	
+	return noErr;
+}
+
+
+class CoreAudioOutput : public GenericOutput
+{
+public:
+	CoreAudioOutput() : m_DeviceID(0)
+	{
+		UInt32 size; Boolean writable; OSStatus err;
+		
+		err = AudioHardwareGetPropertyInfo (kAudioHardwarePropertyDevices, &size, &writable);
+		if (kAudioHardwareNoError != err) return;
+		
+		int m_DeviceListSize = size / sizeof (AudioDeviceID);
+		m_DeviceList = new AudioDeviceID[m_DeviceListSize];
+		
+		err = AudioHardwareGetProperty (kAudioHardwarePropertyDevices, &size, m_DeviceList);
+		if (kAudioHardwareNoError != err)
+		{
+			delete[] m_DeviceList;
+			m_DeviceList = 0;
+			m_DeviceListSize = 0;
+		}
+	}
+	
+	~CoreAudioOutput()
+	{
+		Stop();
+		delete[] m_DeviceList;
+		m_DeviceListSize = 0;
+	}
+	
+	virtual int init(Config&)
+	{
+		return (m_DeviceList && 0 < m_DeviceListSize) ? 0 : -1;
+	}
+	
+	virtual bool Start()
+	{
+		if (!m_DeviceID)
+		{
+			m_DeviceID = m_DeviceList[0];
+			AudioDeviceAddIOProc(m_DeviceID, audioDeviceIOProc, &mInput);
+			AudioDeviceStart(m_DeviceID, audioDeviceIOProc);
+		}
+		return 0;
+	}
+	
+	virtual void Stop()
+	{
+		if (m_DeviceID) {
+			AudioDeviceStop(m_DeviceID, audioDeviceIOProc);
+			m_DeviceID = 0;
+		}
+	}
+	
+private: ;
+	AudioDeviceID* m_DeviceList;
+	unsigned long  m_DeviceListSize;
+	AudioDeviceID  m_DeviceID;
+};
+
+GenericOutput* CreateCoreAudioOutput() { return new CoreAudioOutput; }
+
+
+#pragma mark - CoreMIDI
+
 #include <CoreMIDI/MIDIServices.h>
 
 class CoreMidiInterface : public MidiInterface
@@ -99,6 +208,11 @@ CoreMidiInterface::midiReadProc (const MIDIPacketList *pktlist, void *readProcRe
 	self->HandleMidiPacketList(pktlist);
 }
 
+#pragma mark - CoreAudio
+
+
+
 #else
+GenericOutput* CreateCoreAudioOutput() { return NULL; }
 MidiInterface* CreateCoreMidiInterface() { return NULL; }
 #endif
--- src/drivers/CoreMidiInterface.h
+++ src/drivers/CoreMidiInterface.h
@@ -6,8 +6,10 @@
 #ifndef _CoreMidiInterface_h
 #define _CoreMidiInterface_h
 
+#include "AudioOutput.h"
 #include "MidiInterface.h"
 
+GenericOutput* CreateCoreAudioOutput();
 MidiInterface* CreateCoreMidiInterface();
 
 #endif
