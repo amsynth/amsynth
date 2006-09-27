38b659fd83f87553623f282bdcf35563b7348e0e
Nick Dowell <nickdowell@gmail.com>
2006-09-27 17:18:59 +0000

    added netbeans project file

---
+++ nbproject/Makefile-Default.mk
@@ -0,0 +1,53 @@
+#
+# Gererated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add custumized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=g77
+
+# Include project Makefile
+include amsynthe_Makefile.mk
+
+# Object Files
+OBJECTFILES=
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: 
+	cd .;\
+	${MAKE} -f Makefile
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	cd .;\
+	${MAKE} -f Makefile clean
+
+# Subprojects
+.clean-subprojects:
