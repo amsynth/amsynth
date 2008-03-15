f490f6dd0f9ce3cb43a0a3c78b002b980c3bbd30
Nick Dowell <nickdowell@gmail.com>
2008-03-15 17:39:33 +0000

    can now build a vst under linux :) loads ok under energyXT2

--- src/amsynthVst.cpp
+++ src/amsynthVst.cpp
@@ -13,3 +13,18 @@ extern AudioEffect * createEffectInstance( audioMasterCallback callback )
 {
 	return new AMSynthVst( callback );
 }
+
+#if linux
+
+extern "C" AEffect * main_plugin( audioMasterCallback audioMaster ) asm ("main");
+
+#define main main_plugin
+
+extern "C" AEffect * main( audioMasterCallback audioMaster )
+{ 
+	AudioEffect * effect = createEffectInstance( audioMaster );
+	return effect ? effect->getAeffect() : NULL;
+}
+
+#endif
+
