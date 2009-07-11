76c93ae83846fb5786abbba340955bdc0829f4f0
Nick Dowell <nickdowell@gmail.com>
2009-07-11 14:33:56 +0000

    integrated Adrian Knoth's patch: + use libjack directly (no dlopen anymore) + extend configure.in to add -ljack to LDFLAGS when withJack=true - modified to return 0 from process callback for jackdmp compatibility

--- src/JackOutput.cc
+++ src/JackOutput.cc
@@ -8,176 +8,11 @@
 #include <iostream>
 #include <string.h>
 #include <stdlib.h>
-#include <dlfcn.h>
 
 #define SIZEOF_ARRAY(a) (sizeof(a) / sizeof(a[0]))
 
 #ifdef with_jack
 
-jack_client_t*	(*dl_jack_client_new)				(const char *) = NULL;
-int				(*dl_jack_client_close)				(jack_client_t *);
-const char**	(*dl_jack_get_ports)				(jack_client_t *, const char *, const char *, unsigned long);
-int				(*dl_jack_set_process_callback)		(jack_client_t *, JackProcessCallback, void *);
-int				(*dl_jack_set_buffer_size_callback)	(jack_client_t *, JackBufferSizeCallback, void *);
-int				(*dl_jack_set_sample_rate_callback)	(jack_client_t *, JackSampleRateCallback, void *);
-jack_port_t*	(*dl_jack_port_register)			(jack_client_t *, const char *port_name, const char *, unsigned long, unsigned long);
-void			(*dl_jack_on_shutdown)				(jack_client_t *client, void (*)(void *), void *);
-jack_nframes_t	(*dl_jack_get_sample_rate)			(jack_client_t *);
-jack_nframes_t	(*dl_jack_get_buffer_size)			(jack_client_t *);
-int				(*dl_jack_activate)					(jack_client_t *);
-int				(*dl_jack_deactivate)				(jack_client_t *);
-int				(*dl_jack_connect)					(jack_client_t *, const char *, const char *);
-
-void*			(*dl_jack_port_get_buffer)			(jack_port_t *, jack_nframes_t);
-const char*		(*dl_jack_port_name)				(const jack_port_t *);
-
-bool find_library(const char * searchname, char * result, size_t size)
-{
-	const char * ldconfig_cmd = NULL;
-	const char * ldconfig_paths[] =
-	{
-		"ldconfig",
-		"/sbin/ldconfig",
-		"/usr/sbin/ldconfig",
-	};
-
-	for (size_t i=0; i<SIZEOF_ARRAY(ldconfig_paths); i++)
-	{
-		char cmd[128]; snprintf(cmd, 128, "%s -V", ldconfig_paths[i]);
-		if (system(cmd) == 0)
-		{
-			ldconfig_cmd = ldconfig_paths[i];
-			break;
-		}
-	}
-	if (!ldconfig_cmd)
-	{
-		printf("WARNING : could not find ldconfig executable.\n");
-	}
-
-	char cmd[128]; snprintf(cmd, 128, "%s -p | grep %s", ldconfig_cmd, searchname);
-	FILE * output = popen(cmd, "r");
-	if (output)
-	{
-		const unsigned kBufferSize = 256;
-		char buffer[kBufferSize] = "";
-		fread(buffer, 1, kBufferSize, output);
-		pclose(output);
-		// now search for a string which looks like libXXXX.so
-		const char * lnBeg = strchr(buffer, 'l');
-		const char * lnEnd = strchr(buffer, ' ');
-		const size_t lnLen = lnEnd - lnBeg;
-		strncpy(result, lnBeg, lnLen);
-		result[lnLen] = '\0';
-		return true;
-	}
-	return false;
-}
-
-bool load_libjack()
-{
-	char libjack[128] = "libjack.so";
-	find_library("libjack", libjack, 128);
-	void* handle = dlopen(libjack, RTLD_LAZY);
-	if (NULL == handle) {
-		std::cerr << "cannot load JACK library (" << libjack << ")\n";
-		return false;
-	}
-	
-	dl_jack_client_new = (jack_client_t* (*) (const char *)) dlsym(handle, "jack_client_new");
-	if (NULL == dl_jack_client_new) {
-		std::cerr << "cannot locate 'jack_client_new'\n";
-		return false;
-	}
-	
-	dl_jack_client_close = (int (*)(jack_client_t*)) dlsym(handle, "jack_client_close");
-	if (NULL == dl_jack_client_close) {
-		std::cerr << "cannot locate 'jack_client_close'\n";
-		return false;
-	}
-	
-	dl_jack_get_ports = (const char** (*)(jack_client_t*, const char*, const char*, unsigned long)) dlsym(handle, "jack_get_ports");
-	if (NULL == dl_jack_get_ports) {
-		std::cerr << "cannot locate 'jack_get_ports'\n";
-		return false;
-	}
-	
-	dl_jack_set_process_callback = (int (*)(jack_client_t*, JackProcessCallback, void*)) dlsym(handle, "jack_set_process_callback");
-	if (NULL == dl_jack_set_process_callback) {
-		std::cerr << "cannot locate 'jack_set_process_callback'\n";
-		return false;
-	}
-	
-	dl_jack_set_buffer_size_callback = (int (*)(jack_client_t*, JackBufferSizeCallback bufsize, void*)) dlsym(handle, "jack_set_buffer_size_callback");
-	if (NULL == dl_jack_set_buffer_size_callback) {
-		std::cerr << "cannot locate 'jack_set_buffer_size_callback'\n";
-		return false;
-	}
-	
-	dl_jack_set_sample_rate_callback = (int (*)(jack_client_t*, JackSampleRateCallback, void*)) dlsym(handle, "jack_set_sample_rate_callback");
-	if (NULL == dl_jack_set_sample_rate_callback) {
-		std::cerr << "cannot locate 'jack_set_sample_rate_callback'\n";
-		return false;
-	}
-	
-	dl_jack_port_register = (jack_port_t* (*)(jack_client_t*, const char*, const char*, unsigned long, unsigned long)) dlsym(handle, "jack_port_register");
-	if (NULL == dl_jack_port_register) {
-		std::cerr << "cannot locate 'jack_port_register'\n";
-		return false;
-	}
-	
-	dl_jack_on_shutdown = (void	(*) (jack_client_t *client, void (*)(void *), void *)) dlsym(handle, "jack_on_shutdown");
-	if (NULL == dl_jack_on_shutdown) {
-		std::cerr << "cannot locate 'jack_on_shutdown'\n";
-		return false;
-	}
-	
-	dl_jack_get_sample_rate = (jack_nframes_t (*)(jack_client_t *)) dlsym(handle, "jack_get_sample_rate");
-	if (NULL == dl_jack_get_sample_rate) {
-		std::cerr << "cannot locate 'jack_get_sample_rate'\n";
-		return false;
-	}
-	
-	dl_jack_get_buffer_size = (jack_nframes_t (*)(jack_client_t *)) dlsym(handle, "jack_get_buffer_size");
-	if (NULL == dl_jack_get_buffer_size) {
-		std::cerr << "cannot locate 'jack_get_buffer_size'\n";
-		return false;
-	}
-
-	dl_jack_activate = (int (*)(jack_client_t *)) dlsym(handle, "jack_activate");
-	if (NULL == dl_jack_activate) {
-		std::cerr << "cannot locate 'jack_activate'\n";
-		return false;
-	}
-
-	dl_jack_deactivate = (int (*)(jack_client_t *)) dlsym(handle, "jack_deactivate");
-	if (NULL == dl_jack_deactivate) {
-		std::cerr << "cannot locate 'jack_deactivate'\n";
-		return false;
-	}
-	
-	dl_jack_connect = (int (*)(jack_client_t *, const char *, const char *)) dlsym(handle, "jack_connect");
-	if (NULL == dl_jack_connect) {
-		std::cerr << "cannot locate 'jack_connect'\n";
-		return false;
-	}
-	
-	dl_jack_port_get_buffer = (void* (*)(jack_port_t *, jack_nframes_t)) dlsym(handle, "jack_port_get_buffer");
-	if (NULL == dl_jack_port_get_buffer) {
-		std::cerr << "cannot locate 'jack_port_get_buffer'\n";
-		return false;
-	}
-	
-	dl_jack_port_name = (const char* (*)(const jack_port_t *)) dlsym(handle, "jack_port_name");
-	if (NULL == dl_jack_port_name) {
-		std::cerr << "cannot locate 'jack_port_name'\n";
-		return false;
-	}
-
-	
-	std::cerr << "loaded & initialised " << libjack << " :)\n";
-	return true;
-}
 
 int
 jack_process (jack_nframes_t nframes, void *arg)
@@ -222,16 +57,14 @@ JackOutput::init	( Config & config )
 	initialised = false;
 	client_name = "amSynth";
 	
-	if (!load_libjack()) return -1;
-	
 	
 	// check if there are already any amSynth jack clients...
 	
 	const char **readports;
 	
-	if ( (client = (*dl_jack_client_new)( "amSynth-tmp" )) == 0 ) return -1;
+	if ( (client = jack_client_new( "amSynth-tmp" )) == 0 ) return -1;
 
-	readports = (*dl_jack_get_ports)( client, NULL, NULL, JackPortIsOutput );
+	readports = jack_get_ports( client, NULL, NULL, JackPortIsOutput );
 	
 	int i=0, c=0;
 	while (readports && readports[i])
@@ -240,7 +73,7 @@ JackOutput::init	( Config & config )
 		i++;
 	}
 	c/=2;
-	(*dl_jack_client_close)(client);
+	jack_client_close(client);
 	if (c>0)
 	{
 		char tmp[3];
@@ -252,23 +85,23 @@ JackOutput::init	( Config & config )
 	}
 	
 	/* become a client of the JACK server */
-	if ((client = (*dl_jack_client_new)(client_name.c_str())) == 0)
+	if ((client = jack_client_new(client_name.c_str())) == 0)
 	{
 		error_msg = "jack_client_new() failed";
 		return -1;
 	}
 
-	(*dl_jack_set_process_callback)(client, jack_process, this);
-	(*dl_jack_set_buffer_size_callback)(client, jack_bufsize, this);
-	(*dl_jack_set_sample_rate_callback)(client, jack_srate, this);
-	(*dl_jack_on_shutdown)(client, jack_shutdown, this);
+	jack_set_process_callback(client, jack_process, this);
+	jack_set_buffer_size_callback(client, jack_bufsize, this);
+	jack_set_sample_rate_callback(client, jack_srate, this);
+	jack_on_shutdown(client, jack_shutdown, this);
 
-	sample_rate = (*dl_jack_get_sample_rate)( client );
-	buf_size = (*dl_jack_get_buffer_size)(client);
+	sample_rate = jack_get_sample_rate( client );
+	buf_size = jack_get_buffer_size(client);
 
 	/* create output ports */
-	l_port = (*dl_jack_port_register)(client, "L out", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-	r_port = (*dl_jack_port_register)(client, "R out", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+	l_port = jack_port_register(client, "L out", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+	r_port = jack_port_register(client, "R out", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
 	
 	initialised = true;
 	
@@ -285,8 +118,8 @@ JackOutput::init	( Config & config )
 int
 JackOutput::process (jack_nframes_t nframes)
 {
-	float *lout = (jack_default_audio_sample_t *) (*dl_jack_port_get_buffer)(l_port, nframes);
-	float *rout = (jack_default_audio_sample_t *) (*dl_jack_port_get_buffer)(r_port, nframes);
+	float *lout = (jack_default_audio_sample_t *) jack_port_get_buffer(l_port, nframes);
+	float *rout = (jack_default_audio_sample_t *) jack_port_get_buffer(r_port, nframes);
 	mInput->Process (lout, rout, nframes);
 	return 0;
 }
@@ -317,13 +150,13 @@ JackOutput::Start	()
 #ifdef with_jack
 	if (!initialised) return false;
 	if (!mInput) return false;
-	if ((*dl_jack_activate)(client)) 
+	if (jack_activate(client)) 
 	{
 		std::cerr << "cannot activate JACK client\n";
 		return false;
 	}
-	(*dl_jack_connect)(client, (*dl_jack_port_name)(l_port), "alsa_pcm:playback_1");
-	(*dl_jack_connect)(client, (*dl_jack_port_name)(r_port), "alsa_pcm:playback_2");
+	jack_connect(client, jack_port_name(l_port), "alsa_pcm:playback_1");
+	jack_connect(client, jack_port_name(r_port), "alsa_pcm:playback_2");
 	return true;
 #else
 	return false;
@@ -335,7 +168,7 @@ JackOutput::Stop()
 {
 #ifdef with_jack
 	if (!initialised) return;
-	(*dl_jack_deactivate)(client);
-	(*dl_jack_client_close)(client);
+	jack_deactivate(client);
+	jack_client_close(client);
 #endif
 }
