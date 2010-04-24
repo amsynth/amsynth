c5e26635240b8cd3240882195e00408120166dd7
Nick Dowell <nickdowell@gmail.com>
2010-04-24 09:35:41 +0000

    fixed command_exists() - solves bug 2988697

--- src/GUI/GUI.cc
+++ src/GUI/GUI.cc
@@ -748,16 +748,27 @@ GUI::bank_save_as	( )
 	}
 }
 
+static gchar *which(gchar *command)
+{
+	gint exit_status = -1;
+	gchar *standard_output = NULL;
+	gchar *argv[] = { (gchar *)"/usr/bin/which", (gchar *)command };
+	g_spawn_sync(NULL, argv, NULL, (GSpawnFlags)0, NULL, NULL, &standard_output, NULL, &exit_status, NULL);
+	if (exit_status != 0)
+	{
+		g_free(standard_output);
+		return NULL;
+	}
+	return standard_output;
+}
+
+// returns 0 if executable was found
 int
 GUI::command_exists	(const char *command)
 {
-	string test_command = "/usr/bin/which ";
-	test_command += command;
-	test_command += " &> /dev/null";
-	
-	int res = system (test_command.c_str ());
-	// returns 0 if executable was found
-	return WEXITSTATUS(res);
+	gchar *path = which((gchar *)command);
+	if (path) g_free(path);
+	return path == NULL ? 1 : 0;
 }
 
 void
