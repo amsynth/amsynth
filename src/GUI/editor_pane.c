8192fa65d2105e7adf17163442749db72340be40
Nick Dowell <nickdowell@gmail.com>
2010-07-15 18:16:41 +0000

    First-pass at a data-based skinning system. Skins are zip files, containig a .ini file that defines all the controls & resources.

--- src/GUI/editor_pane.c
+++ src/GUI/editor_pane.c
@@ -10,25 +10,6 @@
 #define SIZEOF_ARRAY( a ) ( sizeof((a)) / sizeof((a)[0]) )
 
 ////////////////////////////////////////////////////////////////////////////////
-//
-// Graphics resources
-//
-extern const char *background_png;
-extern const int   background_png_size;
-//
-extern const char *knob_png;
-extern const int   knob_png_size;
-//
-extern const char *waveform_png;
-extern const int   waveform_png_size;
-//
-extern const char *slider_boost_1_png;
-extern const int   slider_boost_1_png_size;
-//
-extern const char *slider_boost_2_png;
-extern const int   slider_boost_2_png_size;
-//
-////////////////////////////////////////////////////////////////////////////////
 
 static GdkPixbuf *editor_pane_bg = NULL;
 
@@ -36,27 +17,8 @@ static GtkAdjustment *adjustments[kControls_End];
 
 ////////////////////////////////////////////////////////////////////////////////
 
-static GdkPixbuf *
-gdk_pixbuf_new_from_file_data (const void *buf, gsize count)
-{
-	GdkPixbuf *pixbuf = NULL;
-	GdkPixbufLoader *loader = gdk_pixbuf_loader_new ();
-	gdk_pixbuf_loader_write (loader, buf, count, NULL);
-	gdk_pixbuf_loader_close (loader, NULL);
-	pixbuf = g_object_ref (gdk_pixbuf_loader_get_pixbuf (loader));
-	g_object_unref (G_OBJECT (loader));
-	return pixbuf;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-#define TYPE_KNOB	1000
-#define TYPE_POPUP	1001
-#define TYPE_SWITCH	1002
-
 typedef struct
 {
-	gchar bmp_name[32];
 	const char *data;
 	int   length;
 	guint fr_width;
@@ -64,64 +26,6 @@ typedef struct
 	guint fr_count;
 }
 resource_info;
-
-typedef struct
-{
-	guint type;
-	guint x;
-	guint y;
-	gchar bmp_name[32];
-	guint paramid;
-}
-widget_info;
-
-////////////////////////////////////////////////////////////////////////////////
-
-const widget_info widget_layout[] = 
-{
-	{ TYPE_KNOB,	 10,  10, "knob_boost_cut",		kOsc1Pulsewidth		},	// osc 1 pw
-	{ TYPE_POPUP,	 10,  60, "popup_waveforms",	kOsc1Waveform		},	// osc 1 waveform
-
-	{ TYPE_KNOB,	 10, 100, "knob_boost_cut",		kOsc2Pulsewidth		},	// osc 2 pw
-	{ TYPE_POPUP,	 10, 160, "popup_waveforms",	kOsc2Waveform		},	// osc 2 waveform
-	{ TYPE_POPUP,	 10, 190, "popup_waveforms",	kOsc2Sync			},	// osc 2 sync
-	{ TYPE_KNOB,	 70, 100, "knob_boost_cut",		kOsc2Octave			},	// osc 2 octave
-	{ TYPE_KNOB,	 70, 160, "knob_boost_cut",		kOsc2Detune			},	// osc 2 detune
-	
-	{ TYPE_KNOB,	100,  10, "knob_boost_cut",		kOscMix				},	// osc mix
-	{ TYPE_POPUP,	100,  60, "popup_waveforms",	kOscMixRingMod		},	// osc mix ring mod
-
-	{ TYPE_KNOB,	195,  10, "knob_boost_cut",		kFilterCutoff		},	// filter cutoff
-	{ TYPE_KNOB,	245,  10, "knob_boost_cut",		kFilterResonance	},	// filter resonance
-	{ TYPE_KNOB,	295,  10, "knob_boost_cut",		kFilterEnvAmount	},	// filter envelope
-	{ TYPE_KNOB,	170,  70, "knob_boost_cut",		kFilterAttack		},	// filter attack
-	{ TYPE_KNOB,	220,  70, "knob_boost_cut",		kFilterDecay		},	// filter decay
-	{ TYPE_KNOB,	270,  70, "knob_boost_cut",		kFilterSustain		},	// filter sustain
-	{ TYPE_KNOB,	320,  70, "knob_boost_cut",		kFilterRelease		},	// filter release
-
-	{ TYPE_KNOB,	460,  10, "knob_boost_cut",		kMasterVol			},	// amp volume
-	{ TYPE_KNOB,	510,  10, "knob_boost_cut",		kDistortionCrunch	},	// amp distortion
-	{ TYPE_KNOB,	410,  70, "knob_boost_cut",		kAmpAttack			},	// amp attack
-	{ TYPE_KNOB,	460,  70, "knob_boost_cut", 	kAmpDecay			},	// amp decay
-	{ TYPE_KNOB,	510,  70, "knob_boost_cut", 	kAmpSustain			},	// amp sustain
-	{ TYPE_KNOB,	560,  70, "knob_boost_cut", 	kAmpRelease			},	// amp release
-	
-	{ TYPE_KNOB,	410, 300, "slider_boost_2",		kAmpAttack			},	// amp attack
-	{ TYPE_KNOB,	460, 300, "slider_boost_2", 	kAmpDecay			},	// amp decay
-	{ TYPE_KNOB,	510, 300, "slider_boost_2", 	kAmpSustain			},	// amp sustain
-	{ TYPE_KNOB,	560, 300, "slider_boost_2", 	kAmpRelease			},	// amp release
-
-	{ TYPE_KNOB,	170, 160, "knob_boost_cut",		kLFOFreq			},	// lfo freq
-	{ TYPE_POPUP,	170, 210, "popup_waveforms",	kLFOWaveform		},	// lfo waveform
-	{ TYPE_KNOB,	220, 160, "knob_boost_cut",		kFreqModAmount		},	// lfo -> vco
-	{ TYPE_KNOB,	270, 160, "knob_boost_cut",		kFilterModAmount	},	// lfo -> filt
-	{ TYPE_KNOB,	320, 160, "knob_boost_cut",		kAmpModAmount		},	// lfo -> vca
-
-	{ TYPE_KNOB,	410, 160, "knob_boost_cut",		kReverbWet			},	// reverb amount
-	{ TYPE_KNOB,	460, 160, "knob_boost_cut",		kReverbRoomsize		},	// reverb room size
-	{ TYPE_KNOB,	510, 160, "knob_boost_cut",		kReverbWidth		},	// reverb stereo width
-	{ TYPE_KNOB,	560, 160, "knob_boost_cut",		kReverbDamp			},	// reverb damping
-};
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -144,69 +48,205 @@ editor_pane_expose_event_handler (GtkWidget *widget, gpointer data)
 
 ////////////////////////////////////////////////////////////////////////////////
 
+#define HANDLE_GERROR( gerror ) \
+	if (gerror) { \
+		fprintf (stderr, "GError: %s\n", gerror->message); \
+		g_error_free (gerror); \
+		gerror = NULL; \
+		g_assert (FALSE); \
+	}
+
+#define KEY_CONTROL_TYPE		"type"
+#define KEY_CONTROL_TYPE_KNOB	"knob"
+#define KEY_CONTROL_TYPE_POPUP	"popup"
+#define KEY_CONTROL_POS_X		"pos_x"
+#define KEY_CONTROL_POS_Y		"pos_y"
+#define KEY_CONTROL_RESOURCE	"resource"
+#define KEY_CONTROL_PARAM_NAME	"param_name"
+#define KEY_CONTROL_PARAM_NUM	"param_num"
+
+int deldir (const char *dir_path)
+{
+	g_assert (dir_path);
+	gchar *command = g_strdup_printf("rm -r \"%s\"", dir_path);
+	printf ("%s\n", command); return 0;
+	int result = system (command);
+	g_free (command);
+	return result;
+}
+
+gchar *extract_skin (char *skin_file)
+{
+	gchar *tempdir = g_strconcat(g_get_tmp_dir(), "/amsynth.skin.XXXXXXXX", NULL);
+	if (!mkdtemp(tempdir)) {
+		g_message("Failed to create temporary directory. Unable to load skin.");
+		g_free(tempdir);
+		return NULL;
+	}
+	
+	gchar *command = g_strdup_printf("/usr/bin/unzip -qq -o -j \"%s\" -d %s", skin_file, tempdir);
+	int result = system (command);
+	g_free (command);
+	command = NULL;
+	
+	if (result != 0) {
+		g_message("Failed to extract archive. Unable to load skin.");
+		deldir (tempdir);
+		g_free (tempdir);
+		tempdir = NULL;
+	}
+	return tempdir;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 GtkWidget *
 editor_pane_new (GtkAdjustment **adjustments)
 {
-	const resource_info resources[] = 
-	{
-		{ "knob_boost_cut",		knob_png,			knob_png_size,		 		49, 49, 49 },
-		{ "popup_waveforms",	waveform_png,		waveform_png_size,			49, 21,  5 },
-		{ "slider_boost_1",		slider_boost_1_png,	slider_boost_1_png_size, 	31, 49, 31 },
-		{ "slider_boost_2",		slider_boost_2_png,	slider_boost_2_png_size, 	49, 99, 99 },
-	};
-
 	GtkWidget *fixed = gtk_fixed_new ();
+	gtk_widget_set_usize (fixed, 400, 300);
 	
-	editor_pane_bg = gdk_pixbuf_new_from_file_data (background_png, background_png_size);
-	
-	gtk_widget_set_usize (fixed, gdk_pixbuf_get_width (editor_pane_bg), gdk_pixbuf_get_height (editor_pane_bg));
 	g_signal_connect (GTK_OBJECT (fixed), "expose-event", (GtkSignalFunc) editor_pane_expose_event_handler, (gpointer) NULL);
 	
-	int i;
+	gsize i;
 	
-	GData *resdata = NULL;
-	GData *resinfo = NULL;
-	g_datalist_init (&resdata);
-	g_datalist_init (&resinfo);
+//	const gchar *skin_path = "/home/nixx/Code/amsynthe-new-gui-1/src/GUI";
 	
-	for (i=0; i<SIZEOF_ARRAY(resources); i++)
-	{
-		const resource_info *info = resources + i;
-		GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file_data (info->data, info->length);
-		g_datalist_set_data (&resdata, info->bmp_name, (gpointer)pixbuf);
-		g_datalist_set_data (&resinfo, info->bmp_name, (gpointer)info);
+	gchar *skin_path = extract_skin ("/home/nixx/Code/amsynthe-new-gui-1/src/GUI/default.zip");
+	if (skin_path == NULL) {
+		g_message ("Could not load skin :-(");
+		return fixed;
 	}
-
-	const gchar *waveform_strings[] = { "sine", "square / pulse", "saw / triangle", "white noise", "noise + sample & hold" };
 	
-	for (i=0; i<SIZEOF_ARRAY(widget_layout); i++)
 	{
-		const widget_info	*info = widget_layout + i;
-		const resource_info	*res = g_datalist_get_data (&resinfo, info->bmp_name);
-
-		GtkWidget *widget = NULL;
-		GdkPixbuf *frames = GDK_PIXBUF (g_datalist_get_data (&resdata, info->bmp_name));
-		GdkPixbuf *subpixpuf = gdk_pixbuf_new_subpixbuf (editor_pane_bg, info->x, info->y, res->fr_width, res->fr_height);
-		GtkAdjustment *adj = adjustments[info->paramid];
+		GData *resdata = NULL;
+		GData *resinfo = NULL;
+		g_datalist_init (&resdata);
+		g_datalist_init (&resinfo);
+	
+		////////
 		
-		switch (info->type)
+		GError *gerror = NULL;
+		GKeyFile *gkey_file = g_key_file_new ();
+		gchar *ini_path = g_strconcat (skin_path, "/layout.ini", NULL);
+		gboolean ok = g_key_file_load_from_file (gkey_file, ini_path, G_KEY_FILE_NONE, &gerror);
+		g_key_file_set_list_separator (gkey_file, ',');
+		g_free (ini_path); ini_path = NULL;
+		HANDLE_GERROR (gerror);
+		g_assert (ok);
+		
+		////////
+		
 		{
-		case TYPE_KNOB:
-			widget = bitmap_knob_new (adj, frames, res->fr_width, res->fr_height, res->fr_count);
-			bitmap_knob_set_bg (widget, subpixpuf);
-			break;
-		
-		case TYPE_POPUP:
-			widget = bitmap_popup_new (adj, frames, res->fr_width, res->fr_height, res->fr_count);
-			bitmap_popup_set_bg (widget, subpixpuf);
-			bitmap_popup_set_strings (widget, (const gchar **)waveform_strings);
-			break;
+			gchar *bg_name = g_key_file_get_string  (gkey_file, "layout", "background", &gerror); HANDLE_GERROR (gerror); g_strstrip (bg_name);
+			gchar *path = g_strconcat (skin_path, "/", bg_name, NULL);
+			editor_pane_bg = gdk_pixbuf_new_from_file (path, &gerror); HANDLE_GERROR (gerror);
+			g_assert (editor_pane_bg);
+			g_free (bg_name);
+			g_free (path);
+			
+			gtk_widget_set_usize (fixed, gdk_pixbuf_get_width (editor_pane_bg), gdk_pixbuf_get_height (editor_pane_bg));
 		}
 		
-		g_object_unref (G_OBJECT (subpixpuf));
-		gtk_fixed_put (GTK_FIXED (fixed), widget, info->x, info->y);
+		//// Load resources
+		
+		gsize num_resources = 0;
+		gchar **resource_list = g_key_file_get_string_list (gkey_file, "layout", "resources", &num_resources, &gerror); HANDLE_GERROR (gerror);
+		if (resource_list)
+		{
+			for (i=0; i<num_resources; i++)
+			{
+				gchar *resource_name = resource_list[i]; g_strstrip (resource_name);
+				
+				gchar *file = g_key_file_get_string  (gkey_file, resource_name, "file",   &gerror); HANDLE_GERROR (gerror); g_strstrip (file);
+				gint width  = g_key_file_get_integer (gkey_file, resource_name, "width",  &gerror); HANDLE_GERROR (gerror);
+				gint height = g_key_file_get_integer (gkey_file, resource_name, "height", &gerror); HANDLE_GERROR (gerror);
+				gint frames = g_key_file_get_integer (gkey_file, resource_name, "frames", &gerror); HANDLE_GERROR (gerror);
+				
+				gchar *path = g_strconcat (skin_path, "/", file, NULL);
+				printf ("%s\n", path);
+				
+				GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file (path, &gerror); HANDLE_GERROR (gerror); g_assert (pixbuf);
+				
+				g_assert (gdk_pixbuf_get_width (pixbuf) == width || gdk_pixbuf_get_height (pixbuf) == height);
+				g_assert (gdk_pixbuf_get_width (pixbuf) == (width * frames) || gdk_pixbuf_get_height (pixbuf) == (height * frames));
+				
+				resource_info *info = g_malloc0 (sizeof (resource_info));
+				info->fr_width  = width;
+				info->fr_height = height;
+				info->fr_count  = frames;
+				
+				printf ("adding resource '%s'\n", resource_name);
+				g_datalist_set_data (&resdata, resource_name, (gpointer)pixbuf);
+				g_datalist_set_data (&resinfo, resource_name, (gpointer)info);
+				
+				g_free (file);
+				g_free (path);
+			}
+			g_strfreev (resource_list);
+			resource_list = NULL;
+		}
+		
+		//// Create controls
+	
+		gsize num_controls = 0;
+		gchar **control_list = g_key_file_get_string_list (gkey_file, "layout", "controls", &num_controls, &gerror); HANDLE_GERROR (gerror);
+		if (control_list)
+		{
+			for (i=0; i<num_controls; i++)
+			{
+				gchar *control_name = control_list[i]; g_strstrip (control_name);
+				printf ("adding control '%s'\n", control_name);
+				
+				gint pos_x = g_key_file_get_integer (gkey_file, control_name, KEY_CONTROL_POS_X, &gerror); HANDLE_GERROR (gerror);
+				gint pos_y = g_key_file_get_integer (gkey_file, control_name, KEY_CONTROL_POS_Y, &gerror); HANDLE_GERROR (gerror);
+				gchar *resn = g_key_file_get_string (gkey_file, control_name, KEY_CONTROL_RESOURCE, &gerror); HANDLE_GERROR (gerror); g_strstrip (resn);
+				gchar *type = g_key_file_get_string (gkey_file, control_name, KEY_CONTROL_TYPE, &gerror); HANDLE_GERROR (gerror); g_strstrip (type);
+				gchar *param_name = g_key_file_get_string (gkey_file, control_name, KEY_CONTROL_PARAM_NAME, &gerror); HANDLE_GERROR (gerror);
+				const gint param_num = parameter_index_from_name (param_name);
+				g_strstrip (param_name);
+				
+				/////////////////////////
+								
+				GtkWidget *widget = NULL;
+				printf ("using resource '%s'\n", resn);
+				resource_info *res = g_datalist_get_data (&resinfo, resn); g_assert (res);
+				GdkPixbuf *frames = GDK_PIXBUF (g_datalist_get_data (&resdata, resn)); g_assert (frames);
+				GdkPixbuf *subpixpuf = gdk_pixbuf_new_subpixbuf (editor_pane_bg, pos_x, pos_y, res->fr_width, res->fr_height);
+				GtkAdjustment *adj = adjustments[param_num];
+				
+				if (g_strcmp0 (KEY_CONTROL_TYPE_KNOB, type) == 0)
+				{
+					widget = bitmap_knob_new (adj, frames, res->fr_width, res->fr_height, res->fr_count);
+					bitmap_knob_set_bg (widget, subpixpuf);
+				}
+				else if (g_strcmp0 (KEY_CONTROL_TYPE_POPUP, type) == 0)
+				{
+					gsize nstrings = 0;
+					gchar **strings = g_key_file_get_string_list (gkey_file, control_name, "strings", &nstrings, &gerror); HANDLE_GERROR (gerror);
+					widget = bitmap_popup_new (adj, frames, res->fr_width, res->fr_height, res->fr_count);
+					bitmap_popup_set_strings (widget, (const char **)strings);	
+					bitmap_popup_set_bg (widget, subpixpuf);
+					g_strfreev (strings);
+				}
+				
+				g_object_unref (G_OBJECT (subpixpuf));
+				gtk_fixed_put (GTK_FIXED (fixed), widget, pos_x, pos_y);
+				
+				g_free (resn);
+				g_free (type);
+				g_free (param_name);
+			}
+			g_strfreev (control_list);
+			control_list = NULL;
+		}
+	
+		g_key_file_free (gkey_file);
 	}
 	
+	deldir (skin_path);
+	g_free (skin_path);
+
 	return fixed;
 }
 
