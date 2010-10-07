05467b4cbc71e030e244d7d56ab383b12484fe35
Nick Dowell <nickdowell@gmail.com>
2010-10-07 21:47:20 +0000

    Added lash test to testBuildConfigurations.sh, use make checkdist instead of default target

--- testBuildConfigurations.sh
+++ testBuildConfigurations.sh
@@ -3,7 +3,7 @@
 function testOptions()
 {
 	echo "[+] testing configuration: $1"
-	./configure $1 1>/dev/null && make clean all 1>/dev/null
+	./configure $1 1>/dev/null && make distcheck 1>/dev/null
 	if [ $? -ne 0 ]; then
 		echo "************************************************";
 		echo ""
@@ -20,7 +20,7 @@ testOptions "--with-jack=yes --with-alsa=no  --with-sndfile=no  --with-oss=no"
 testOptions "--with-jack=no  --with-alsa=yes --with-sndfile=no  --with-oss=no"
 testOptions "--with-jack=no  --with-alsa=no  --with-sndfile=yes --with-oss=no"
 testOptions "--with-jack=no  --with-alsa=no  --with-sndfile=no  --with-oss=yes"
-testOptions "--with-jack=yes --with-alsa=yes --with-sndfile=yes --with-oss=yes"
+testOptions "--with-jack=yes --with-alsa=yes --with-sndfile=yes --with-oss=yes --with-lash=no"
 testOptions "--enable-realtime"
 testOptions "--enable-profile"
 testOptions "--enable-debug"
