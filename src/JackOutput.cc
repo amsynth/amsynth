b39a0bd00787e465b85f99596a90b4677d5db44c
Nick Dowell <nickdowell@gmail.com>
2010-10-12 09:47:39 +0000

    Tidied up JackOutput - now using jack_client_open, removed cruft

--- src/JackOutput.cc
+++ src/JackOutput.cc
@@ -1,5 +1,5 @@
 /* amSynth
- * (c) 2001-2007 Nick Dowell
+ * (c) 2001-2010 Nick Dowell
  **/
 
 #include "JackOutput.h"
@@ -10,113 +10,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZEOF_ARRAY(a) (sizeof(a) / sizeof(a[0]))
-
-#ifdef with_jack
-
-
-int
-jack_process (jack_nframes_t nframes, void *arg)
-{
-	JackOutput *client = (JackOutput *) arg;
-	return client->process (nframes);
-}
-
-int
-jack_bufsize (jack_nframes_t nframes, void *arg)
-{
-	JackOutput *client = (JackOutput *) arg;
-	return client->bufsize (nframes);
-}
-
-int
-jack_srate (jack_nframes_t nframes, void *arg)
-{
-	JackOutput *client = (JackOutput *) arg;
-	return client->srate (nframes);
-}
-
-void
-jack_shutdown (void *arg)
-{
-	JackOutput *client = (JackOutput *) arg;
-	client->shutdown ();
-}
-
-void
-jack_error	(const char* msg)
-{
-	std::cerr << msg << std::endl;
-}
-
-#endif
-
-JackOutput::JackOutput()
-{
-	initialised = false;
-	client_name = "amSynth";	
-}
-
 int
 JackOutput::init	( Config & config )
 {
 #ifdef with_jack
-	if (initialised)
+	if (client) // already initialised
 		return 0;
-	
-	// check if there are already any amSynth jack clients...
-	
-	const char **readports;
-	
-	if ( (client = jack_client_new( "amSynth-tmp" )) == 0 ) return -1;
 
-	readports = jack_get_ports( client, NULL, NULL, JackPortIsOutput );
-	
-	int i=0, c=0;
-	while (readports && readports[i])
-	{
-		if (strncmp( readports[i], "amSynth", 7 )==0) c++;
-		i++;
-	}
-	c/=2;
-	jack_client_close(client);
-	if (c>0)
-	{
-		char tmp[3];
-		sprintf( tmp, "%d", c );
-		
-		client_name += " (";
-		client_name += string( tmp );
-		client_name += ")";
-	}
-	
-	/* become a client of the JACK server */
-	if ((client = jack_client_new(client_name.c_str())) == 0)
-	{
-		error_msg = "jack_client_new() failed";
+	client = jack_client_open("amsynth", JackNoStartServer, NULL);
+	if (!client) {
+		error_msg = "jack_client_open() failed";
 		return -1;
 	}
-
-	jack_set_process_callback(client, jack_process, this);
-	jack_set_buffer_size_callback(client, jack_bufsize, this);
-	jack_set_sample_rate_callback(client, jack_srate, this);
-	jack_on_shutdown(client, jack_shutdown, this);
-
-	sample_rate = jack_get_sample_rate( client );
-	buf_size = jack_get_buffer_size(client);
+	
+	jack_set_process_callback(client, &JackOutput::process, this);
 
 	/* create output ports */
 	l_port = jack_port_register(client, "L out", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
 	r_port = jack_port_register(client, "R out", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
 	
-	initialised = true;
-	
-	config.sample_rate = sample_rate;
-	config.buffer_size = buf_size;
 	config.current_audio_driver = "JACK";
-	config.jack_client_name = (jack_get_client_name) ?
-		std::string(jack_get_client_name(client)) :
-		client_name;
+	config.sample_rate = jack_get_sample_rate(client);
+	config.buffer_size = jack_get_buffer_size(client);
+	config.jack_client_name = std::string(jack_get_client_name(client));
 	
 	return 0;
 #endif
@@ -126,31 +42,13 @@ JackOutput::init	( Config & config )
 
 #ifdef with_jack
 int
-JackOutput::process (jack_nframes_t nframes)
+JackOutput::process (jack_nframes_t nframes, void *arg)
 {
-	float *lout = (jack_default_audio_sample_t *) jack_port_get_buffer(l_port, nframes);
-	float *rout = (jack_default_audio_sample_t *) jack_port_get_buffer(r_port, nframes);
-	mInput->Process (lout, rout, nframes);
+	JackOutput *self = (JackOutput *)arg;
+	float *lout = (jack_default_audio_sample_t *) jack_port_get_buffer(self->l_port, nframes);
+	float *rout = (jack_default_audio_sample_t *) jack_port_get_buffer(self->r_port, nframes);
+	self->mInput->Process (lout, rout, nframes);
 	return 0;
-}
-
-int
-JackOutput::bufsize (jack_nframes_t nframes)
-{
-	buf_size = nframes;
-	return 0;
-}
-
-int
-JackOutput::srate (jack_nframes_t nframes)
-{
-	sample_rate = nframes;
-	return 0;
-}
-
-void
-JackOutput::shutdown ()
-{
 }
 #endif
 
@@ -158,7 +56,7 @@ bool
 JackOutput::Start	()
 {
 #ifdef with_jack
-	if (!initialised) return false;
+	if (!client) return false;
 	if (!mInput) return false;
 	if (jack_activate(client)) 
 	{
@@ -177,8 +75,9 @@ void
 JackOutput::Stop()
 {
 #ifdef with_jack
-	if (!initialised) return;
+	if (!client) return;
 	jack_deactivate(client);
 	jack_client_close(client);
+	client = 0;
 #endif
 }
--- src/JackOutput.h
+++ src/JackOutput.h
@@ -1,5 +1,5 @@
 /* amSynth
- * (c) 2001-2005 Nick Dowell
+ * (c) 2001-2010 Nick Dowell
  */
 
 #ifndef _JACK_OUTPUT_H
@@ -15,8 +15,6 @@
 class JackOutput : public GenericOutput {
 
 public:
-				JackOutput	( );
-	virtual		~JackOutput	( ) {};
 	
 	int			init		( Config & config );// returns 0 on success
 	bool		Start		();
@@ -28,27 +26,18 @@ public:
 	void		setOutputFile	( string file )	{ wavoutfile = file; };
 	string		getOutputFile	( )		{ return wavoutfile; };
 
-	const char*	getTitle	( )	{ return client_name.c_str(); };
-
 #ifdef with_jack
-	int			process		(jack_nframes_t nframes);
-	int			bufsize		(jack_nframes_t nframes);
-	int			srate		(jack_nframes_t nframes);
-	void		shutdown	();
+	static int process(jack_nframes_t nframes, void *arg);
 #endif
 private:
 	int	running;
-	int	channels;
 	string	wavoutfile;
 	int	recording;
-	string	client_name, error_msg;
+	string	error_msg;
 #ifdef with_jack
 	jack_port_t 	*l_port, *r_port;
 	jack_client_t 	*client;
 #endif
-	int		sample_rate;
-	int		buf_size;
-	bool	initialised;
 };
 
 #endif				// _JACK_OUTPUT_H
