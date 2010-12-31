e0ba0e4d9b8a359c33b27b8a94db950075e952ae
Nick Dowell <nickdowell@gmail.com>
2010-12-31 16:08:41 +0000

    Fix building without LASH

--- src/lash.c
+++ src/lash.c
@@ -133,7 +133,8 @@ void amsynth_lash_poll_events()
 
 #else
 
-void amsynth_lash_init(int *argc, char ***argv) {}
+void amsynth_lash_process_args(int *argc, char ***argv) {}
+void amsynth_lash_init() {}
 void amsynth_lash_poll_events() {}
 void amsynth_lash_set_jack_client_name(const char *name) {}
 void amsynth_lash_set_alsa_client_id(unsigned char id) {}
