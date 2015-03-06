717060169ca6361d7dbba0639f401b08ed5d1c2c
David Robillard <d@drobilla.net>
2015-03-05 23:54:46 -0500

    Fix compilation on Debian Jessie (GCC 4.9).
    NULL is defined in <cstddef>.

--- src/drivers/ALSAAudioDriver.cc
+++ src/drivers/ALSAAudioDriver.cc
@@ -23,6 +23,8 @@
 #include "../../config.h"
 #endif
 
+#include <cstddef>
+
 #include "ALSAAudioDriver.h"
 
 #ifdef WITH_ALSA
--- src/drivers/OSSAudioDriver.cc
+++ src/drivers/OSSAudioDriver.cc
@@ -23,6 +23,8 @@
 #include "../../config.h"
 #endif
 
+#include <cstddef>
+
 #include "OSSAudioDriver.h"
 
 #ifdef WITH_OSS
