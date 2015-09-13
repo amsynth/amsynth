7ccee3f1e2739502ffcf5c62db1c40aad31e0f3e
Nick Dowell <nick@nickdowell.com>
2015-09-13 17:03:20 +0100

    tidy up top level source tree

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -e
 
-if [ -f .git/hooks/pre-commit.sample -a ! -f .git/hooks/pre-commit ] ; then
-        cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \
-        chmod +x .git/hooks/pre-commit && \
-        echo "Activated pre-commit hook."
-fi
-
 autoreconf --install --symlink
 
 libdir() {
