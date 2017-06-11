29f2937b87dcd2b490b796364fba34bab1ede99d
Nick Dowell <nick@nickdowell.com>
2017-06-11 09:23:52 +0100

    Save & restore tuning config for LV2 plug-in

--- src/amsynth_lv2.cpp
+++ src/amsynth_lv2.cpp
@@ -24,6 +24,7 @@
 #include "Preset.h"
 #include "Synthesizer.h"
 
+#include "lv2/lv2plug.in/ns/ext/atom/atom.h"
 #include "lv2/lv2plug.in/ns/ext/atom/forge.h"
 #include "lv2/lv2plug.in/ns/ext/midi/midi.h"
 #include "lv2/lv2plug.in/ns/ext/patch/patch.h"
@@ -31,7 +32,9 @@
 #include "lv2/lv2plug.in/ns/ext/worker/worker.h"
 #include "lv2/lv2plug.in/ns/lv2core/lv2_util.h"
 
-#include <stdio.h>
+#include <cstdio>
+#include <map>
+#include <string>
 
 #ifdef DEBUG
 #define LOG_FUNCTION_CALL()		fprintf(stderr, AMSYNTH_LV2_URI " %s\n", __FUNCTION__)
@@ -40,13 +43,16 @@
 #endif
 
 struct amsynth_wrapper {
-	Synthesizer *synth;
+	amsynth_wrapper() : schedule(0), control_port(0), midi_in_port(0), out_l(0), out_r(0) {}
+
+	Synthesizer synth;
 
 	struct {
 		LV2_URID midiEvent;
 		LV2_URID patch_Set;
 		LV2_URID patch_property;
 		LV2_URID patch_value;
+		LV2_URID atom_String;
 		LV2_URID amsynth_kbm_file;
 		LV2_URID amsynth_scl_file;
 	} uris;
@@ -59,6 +65,27 @@ struct amsynth_wrapper {
 	float *out_l;
 	float *out_r;
 	float *param_ports[kAmsynthParameterCount];
+
+	std::map<LV2_URID, std::string> patch_values;
+
+	void patchSet(LV2_URID urid, const char *value)
+	{
+		patch_values[urid] = (std::string) value;
+
+		if (urid == uris.amsynth_kbm_file) {
+			if (strlen(value))
+				synth.loadTuningKeymap(value);
+			else
+				synth.defaultTuning();
+		}
+
+		if (urid == uris.amsynth_scl_file) {
+			if (strlen(value))
+				synth.loadTuningScale(value);
+			else
+				synth.defaultTuning();
+		}
+	}
 };
 
 static LV2_Handle
@@ -66,7 +93,7 @@ lv2_instantiate(const struct _LV2_Descriptor *descriptor, double sample_rate, co
 {
 	LOG_FUNCTION_CALL();
 
-	amsynth_wrapper *a = (amsynth_wrapper *)calloc(1, sizeof(amsynth_wrapper));
+	amsynth_wrapper *a = new amsynth_wrapper;
 
 	LV2_URID_Map *urid_map = NULL;
 	const char *missing = lv2_features_query(
@@ -79,13 +106,13 @@ lv2_instantiate(const struct _LV2_Descriptor *descriptor, double sample_rate, co
 		return NULL;
 	}
 
-	a->synth = new Synthesizer;
-	a->synth->setSampleRate((int)sample_rate);
+	a->synth.setSampleRate((int)sample_rate);
 
 	a->uris.midiEvent          = urid_map->map(urid_map->handle, LV2_MIDI__MidiEvent);
 	a->uris.patch_Set          = urid_map->map(urid_map->handle, LV2_PATCH__Set);
 	a->uris.patch_property     = urid_map->map(urid_map->handle, LV2_PATCH__property);
 	a->uris.patch_value        = urid_map->map(urid_map->handle, LV2_PATCH__value);
+	a->uris.atom_String        = urid_map->map(urid_map->handle, LV2_ATOM__String);
 	a->uris.amsynth_kbm_file   = urid_map->map(urid_map->handle, AMSYNTH__tuning_kbm_file);
 	a->uris.amsynth_scl_file   = urid_map->map(urid_map->handle, AMSYNTH__tuning_scl_file);
 
@@ -99,9 +126,7 @@ lv2_cleanup(LV2_Handle instance)
 {
 	LOG_FUNCTION_CALL();
 
-	amsynth_wrapper * a = (amsynth_wrapper *) instance;
-	delete a->synth;
-	free ((void *)a);
+	delete (amsynth_wrapper *) instance;
 }
 
 static void
@@ -171,14 +196,14 @@ lv2_run(LV2_Handle instance, uint32_t sample_count)
 	for (unsigned i=0; i<kAmsynthParameterCount; i++) {
 		const float *host_value = a->param_ports[i];
 		if (host_value != NULL) {
-			if (a->synth->getParameterValue((Param)i) != *host_value) {
-				a->synth->setParameterValue((Param)i, *host_value);
+			if (a->synth.getParameterValue((Param)i) != *host_value) {
+				a->synth.setParameterValue((Param)i, *host_value);
 			}
 		}
 	}
 
 	std::vector<amsynth_midi_cc_t> midi_out;
-	a->synth->process(sample_count, midi_events, midi_out, a->out_l, a->out_r);
+	a->synth.process(sample_count, midi_events, midi_out, a->out_l, a->out_r);
 }
 
 static LV2_State_Status
@@ -190,9 +215,17 @@ save(LV2_Handle                instance,
 {
 	LOG_FUNCTION_CALL();
 
+	amsynth_wrapper * a = (amsynth_wrapper *) instance;
+
 	// host takes care of saving port values
 
-	// TODO: store current tuning setup
+	std::map<LV2_URID, std::string>::const_iterator it;
+	for (it = a->patch_values.begin(); it != a->patch_values.end(); ++it) {
+		if (!it->second.length())
+			continue;
+		store(handle, it->first, it->second.c_str(), it->second.length() + 1,
+			  a->uris.atom_String, LV2_STATE_IS_POD | LV2_STATE_IS_PORTABLE);
+	}
 
 	return LV2_STATE_SUCCESS;
 }
@@ -206,9 +239,19 @@ restore(LV2_Handle                  instance,
 {
 	LOG_FUNCTION_CALL();
 
+	amsynth_wrapper * a = (amsynth_wrapper *) instance;
+
 	// host takes care of restoring port values
 
-	// TODO: restore current tuning setup
+	std::vector<LV2_URID> urids { a->uris.amsynth_kbm_file, a->uris.amsynth_scl_file };
+	std::vector<LV2_URID>::iterator it;
+	for (it = urids.begin(); it != urids.end(); ++it) {
+		size_t size = 0; uint32_t type = 0, vflags = 0;
+		const void *value = retrieve(handle, *it, &size, &type, &vflags);
+		if (value && type == a->uris.atom_String) {
+			a->patchSet(*it, (const char *) value);
+		}
+	}
 
 	return LV2_STATE_SUCCESS;
 }
@@ -232,27 +275,7 @@ work(LV2_Handle                  instance,
 							a->uris.patch_property, &property,
 							a->uris.patch_value, &value,
 							0);
-
-		LV2_URID urid = ((LV2_Atom_URID *) (void *) property)->body;
-		const char *filename = (const char *) LV2_ATOM_BODY_CONST(value);
-
-		fprintf(stderr, "amsynth lv2 worker: urid=%d filename=%s\n", urid, filename);
-
-		if (urid == a->uris.amsynth_kbm_file) {
-			if (strlen(filename)) {
-				a->synth->loadTuningKeymap(filename);
-			} else {
-				a->synth->defaultTuning();
-			}
-		}
-
-		if (urid == a->uris.amsynth_scl_file) {
-			if (strlen(filename)) {
-				a->synth->loadTuningScale(filename);
-			} else {
-				a->synth->defaultTuning();
-			}
-		}
+		a->patchSet(((LV2_Atom_URID *) (void *) property)->body, (const char *) LV2_ATOM_BODY_CONST(value));
 	}
 
 	return LV2_WORKER_SUCCESS;
