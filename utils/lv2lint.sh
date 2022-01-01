fcfee71ae0a9787d67ae283f39776d59a686bd27
Nick Dowell <nick@nickdowell.com>
2022-01-01 11:50:55 +0000

    Fix lv2lint.sh

--- utils/lv2lint.sh
+++ utils/lv2lint.sh
@@ -5,10 +5,10 @@ set -o xtrace
 
 cd /tmp
 
-if [ ! -f lv2lint-0.8.0/x86_64-linux-gnu-stretch/bin/lv2lint ]; then
-  wget --quiet https://dl.open-music-kontrollers.ch/lv2lint/stable/lv2lint-0.8.0.zip
-  unzip -qq lv2lint-0.8.0.zip lv2lint-0.8.0/x86_64-linux-gnu-stretch/bin/lv2lint
-  rm lv2lint-0.8.0.zip
+if [ ! -f lv2lint-0.6.0/x86_64-linux-gnu-stretch/bin/lv2lint ]; then
+  wget --quiet https://dl.open-music-kontrollers.ch/lv2lint/stable/lv2lint-0.6.0.zip
+  unzip -qq lv2lint-0.6.0.zip lv2lint-0.6.0/x86_64-linux-gnu-stretch/bin/lv2lint
+  rm lv2lint-0.6.0.zip
 fi
 
-./lv2lint-0.8.0/x86_64-linux-gnu-stretch/bin/lv2lint -d -M pack http://code.google.com/p/amsynth/amsynth
+./lv2lint-0.6.0/x86_64-linux-gnu-stretch/bin/lv2lint -d -M pack http://code.google.com/p/amsynth/amsynth
