16caa2cc50aa9054fdeac04b3e939538d93e14c9
Nick Dowell <nick@nickdowell.com>
2022-06-22 06:47:29 +0100

    Check for AX_CXX_COMPILE_STDCXX_11 in autogen.sh
    Resolves #208

--- autogen.sh
+++ autogen.sh
@@ -12,6 +12,15 @@ autoreconf --verbose --force --install
 intltoolize --force
 cd -
 
+if ! grep -q AX_CXX_COMPILE_STDCXX_11 aclocal.m4; then
+	echo
+	echo "ERROR: The AX_CXX_COMPILE_STDCXX_11 macro from the GNU Autoconf Archive was not found."
+	echo
+	echo "Try installing the autoconf-archive package and re-running autogen.sh"
+	echo
+	exit 1
+fi
+
 echo
 echo "----------------------------------------------------------------"
 echo "Initialized build system. For a common configuration please run:"
